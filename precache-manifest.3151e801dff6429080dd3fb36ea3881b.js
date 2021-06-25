2bb521e7aea5a951e2392823d6d2dbfe11ce7adf
abikaram <abikaramad@gmail.com>
2021-06-25 12:50:22 -0400

    Updates

---
+++ precache-manifest.3151e801dff6429080dd3fb36ea3881b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26396ff2ab8fdfaa22f747066ca8c36",
+    "url": "/BitcoinDisplayRho/index.html"
+  },
+  {
+    "revision": "f0ac36bdb3d0d7740cc8",
+    "url": "/BitcoinDisplayRho/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "8b89d99ef7d51adf25ba",
+    "url": "/BitcoinDisplayRho/static/css/main.40d99dc7.chunk.css"
+  },
+  {
+    "revision": "f0ac36bdb3d0d7740cc8",
+    "url": "/BitcoinDisplayRho/static/js/2.2c241f98.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/BitcoinDisplayRho/static/js/2.2c241f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b89d99ef7d51adf25ba",
+    "url": "/BitcoinDisplayRho/static/js/main.1d4b1dd7.chunk.js"
+  },
+  {
+    "revision": "4967f0c65bef85852da4",
+    "url": "/BitcoinDisplayRho/static/js/runtime-main.21ae7883.js"
+  },
+  {
+    "revision": "fe2d1cb0ac06428848867f6fe12e1e69",
+    "url": "/BitcoinDisplayRho/static/media/btc_icon.fe2d1cb0.png"
+  }
+]);
